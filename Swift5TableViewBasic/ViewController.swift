957503a2deb5234d828bde22a69c6b40280fe82d
vagrant <vagrant@localhost.localdomain>
2021-01-20 02:57:16 +0000

    first commit

---
+++ Swift5TableViewBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5TableViewBasic
+//
+//  Created by 長井崚介 on 2021/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
